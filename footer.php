338309c6914cc684a756c1d603e6b2b4a3d77f51
M5561 <melih.anik@student.graafschapcollege.nl>
2022-04-29 13:28:54 +0200

    footer

---
+++ footer.php
@@ -0,0 +1,62 @@
+
+<style>
+    /* Footer */
+
+footer{
+    background-color: gray;
+    width: 100%;
+    color: #fff;
+    padding: 60px 0px;
+}
+
+footer .container{
+    display: flex;
+    align-content: center;
+}
+
+footer .container .column{
+    margin-right: 120px;
+}
+
+footer .container .column p{
+    margin: 10px 0px;
+}
+
+footer .container .column ul{
+    padding: 0px;
+    list-style: none;
+}
+
+footer .container .column li a{
+    color: #fff;
+    text-decoration: none;
+    margin: 10px 0px;
+}
+
+</style>
+    <!--  Start footer -->
+    <footer>
+        <div class="container">
+            <div class="column column1">
+            <!-- gegevens voor contact -->
+                <h3>Contact</h3>
+                <p>Adres: Walstraat 1, 7061 CA Terborg</p>
+                <p>Telefoon: 0315 632 042</p>
+                <p>Email: soarpc@soar.nl</p>
+            </div>
+            <div class="column column2">
+                <h3>Links</h3>
+                <!-- snelle links naar paginas -->
+                <ul class="footerNav">
+                    <li><a href="#">Link1</a></li>
+                    <li><a href="#">Link2</a></li>
+                    <li><a href="#">Link3</a></li>
+                    <li><a href="#">Link4</a></li>
+                </ul>
+            </div>
+        </div>
+
+    </footer>
+    <!--  End footer -->
+    </body>
+</html>
