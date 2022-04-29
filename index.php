a22b8b9bc65f1740c540a01f725075c6f676164f
M5561 <melih.anik@student.graafschapcollege.nl>
2022-04-29 13:17:22 +0200

    jo

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-<?php
 <h1>ik zit dagelijk te facebooken. Face.</h1>
 ?>
-=======
 <!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,8 +9,7 @@
 </head>
 <body>
     <?php
-        $een = 1;
+
     ?>
 </body>
 </html>
->>>>>>> 16a1baeb9c7832b7dda160cf54e1d299e120f3e8
