e2eea910934e9563ef192c44cdf3bd3725c5dad2
PigFish <PigFish3@hotmail.com>
2022-04-29 13:46:42 +0200

    idk anymore

--- header.php
+++ header.php
@@ -9,25 +9,11 @@
     <title>Document</title>
 </head>
 <body>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="Css/style.css">
-</head>
-<body>
 <header>
     <nav>
             <img src="Image/Gereedschap.png" alt="">
         <ul>
-            <div class="container">
                 <a href="index.php">Home</a>
-            </div>
         </ul>
     </nav>
 </header>
-</body>
-</html>
