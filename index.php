3c30b95e382f7fda467134f55ed7ed801108e746
JarnoGombert <jarnogombert@gmail.com>
2022-05-13 11:13:07 +0200

    inlog

--- index.php
+++ index.php
@@ -7,8 +7,37 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-        $een = 1;
-    ?>
+<div class="page">
+    <div class="home">
+        <h1><a style="text-decoration: none; color: black;" href="#">Beheer</a></h1> 
+        <h4>Alleen voor bevoegden</h4>
+    </div>    
+    <div class="Beheer">
+        <form action="#" method="POST">
+            <div style="display: flex; justify-content: center; margin-right: 20px">
+                <label for="">E-mail</label>
+                <input name="email" id="input" type="email" required>
+            </div>
+            <div style="padding-top: 5px; display: flex; justify-content: center;">
+                <label for="">Wachtwoord</label>
+                <input name="wachtwoord" class="input" id="Inputt" type="password" required>
+                <input type="checkbox" name="Inputt" onclick="myFunction()">
+            </div>
+            <div style="padding-top: 10px; display: flex; justify-content: center; margin-left: 120px;">
+                <input id="input" name="login" type="submit" value="Login">
+            </div>
+        </form>
+    </div>
+<script>
+    // Dit is voor het wachtwoord checken met een checkbox
+function myFunction() {
+  var x = document.getElementById("Inputt");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}  
+</script>
 </body>
 </html>
