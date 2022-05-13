8de5bcf14bc26d4f9a2409e22726b6103c144efc
JarnoGombert <jarnogombert@gmail.com>
2022-05-13 12:12:57 +0200

    hallo

--- db_functions.php
+++ db_functions.php
@@ -1,47 +1,41 @@
-<!-- hier include ik de database defines -->
 <?php
 include 'database.php';
-
 function db_connect()
 {
-    try {
-        $dbString = "mysql:host=".DB_HOST.";dbname=".DB_NAME;
-
-        $db = new PDO($dbString, DB_USER, DB_PASSWORD);
-        // set the PDO error mode to exception
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        return $db;
-      } catch(PDOException $e) {
-        die("Connection failed: " . $e->getMessage());
-      }
+    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    return $mysqli;
 }
-
+    
 function db_getData($query)
 {
-    $DB = db_connect();
-    $result = $DB->query($query);
-    $DB = null;
+    $mysqli = db_connect();
+    $result = $mysqli->query($query);
+    $mysqli->close();
     return $result;
-} 
+}
 
-function db_insertData($query) {
-    try{
-        $db = db_connect();
-        $queryPDO = $db->prepare($query);
-        $queryPDO->execute();
-        $db = null;
-        return $queryPDO;
-    } catch(PDOException $e) {
-        die("Error: " . $e->getMessage());
-    }
+function db_insertData($query)
+{
+    $mysqli = db_connect();
+    $result = $mysqli->query($query);
+    if ($result === TRUE) 
+    {
+       // Return row id for succes
+        return mysqli_insert_id($mysqli);
+    } 
+    else 
+    {
+        $result = "Error: " . $query . "<br>" . $mysqli->error;
+     }
+    $mysqli->close();
+    return $result;
 }
 
 function getUser($email,$password)
 {
-    $user = db_getData("SELECT * FROM beheerder WHERE E-mail = '$email' AND Wachtwoord = '$password'");
+    $user = db_getData("SELECT * FROM beheerder WHERE 'E-mail' = '$email' AND 'Wachtwoord' = '$password'");
     //PDO: rowCount
-    if ($user->rowCount() > 0 )
+    if (mysqli_num_rows($user) > 0 )
     {
         // User found, return user data
         return $user;
