4b2cc98f795c80f57e8895c7e14918cf8576ef52
JarnoGombert <jarnogombert@gmail.com>
2022-05-20 13:16:14 +0200

    footer aangepast

--- footer.php
+++ footer.php
@@ -1,23 +1,25 @@
 
 
     <!--  Start footer -->
-        <footer class="container">
-            <div class="column column1">
-            <!-- gegevens voor contact -->
-                <h3>Contact</h3>
-                <p>Adres: Walstraat 1, 7061 CA Terborg</p>
-                <p>Telefoon: 0315 632 042</p>
-                <p>Email: Harrybv@gmail.nl</p>
-            </div>
-            <div class="column column2">
-                <h3>Links</h3>
-                <!-- snelle links naar paginas -->
-                <ul class="footerNav">
-                    <li><a href="#">Instagram</a></li>
-                    <li><a href="#">Facebook</a></li>
-                    <li><a href="#">Twitter</a></li>
-                    <li><a href="https://youtu.be/dQw4w9WgXcQ">Youtube</a></li>
-                </ul>
+        <footer>
+            <div hier bepaalde width in en dan de andere 2 divs display: space-evenly>
+                <div class="column column1">
+                <!-- gegevens voor contact -->
+                    <h3>Contact</h3>
+                    <p>Adres: Walstraat 1, 7061 CA Terborg</p>
+                    <p>Telefoon: 0315 632 042</p>
+                    <p>Email: Harrybv@gmail.nl</p>
+                </div>
+                <div class="column column2">
+                    <h3>Links</h3>
+                    <!-- snelle links naar paginas -->
+                    <ul class="footerNav">
+                        <li><a href="#">Instagram</a></li>
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="https://youtu.be/dQw4w9WgXcQ">Youtube</a></li>
+                    </ul>
+                </div>
             </div>
         </footer>
 
